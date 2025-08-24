5c6a83d5679a25140dccd9f1117f0ef334b2219f
confupanda <c.moralessotov@gmail.com>
2025-08-24 19:49:51 -0400

    Add files via upload

---
+++ Morales_Cristian_CalculoCuadrado.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main (){
+
+    int numero, resultado = 0;
+
+
+    cout<<"\ningrese un numero para elevar al cuadrado: "; cin>>numero;
+
+    resultado = pow(numero,2);
+
+    cout<<"\nel resultado de su numero es: "<<resultado<<endl;
+
+
+
+
+    return 0;
+}
